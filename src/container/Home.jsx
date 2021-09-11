d2c990c7179e691110dc4742e0d658bf50daf46c
Harrison <harrisonjones5678@gmail.com>
2021-09-10 23:05:28 -0400

    add navigation styles, add google font, add reset sheet and stylesheets

---
+++ src/container/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import Navbar from "components/Navbar";
+
+function Home(props) {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+Home.propTypes = {};
+
+export default Home;
