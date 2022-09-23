4fe851ba5a5a8be392bbaad64002e33741f8d8c0
Harrison Jones <harrisonjones5678@gmail.com>
2022-09-23 16:11:41 -0400

    Add tests that check both mobile and desktop views (#3)

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -5,11 +5,23 @@ import Navbar from './Navbar'
 describe('<Navbar />', () => {
 
   const hideMobileProductsMenuMock = jest.fn();
+  const { rerender } = render(<Navbar hideMobileProductsMenu={hideMobileProductsMenuMock} hideMobileProducts isDesktop />)
 
-  render(<Navbar hideMobileProductsMenu={hideMobileProductsMenuMock} hideMobileProducts isDesktop />)
 
   it('should load navbar with logo', () => {
     const audiophileLogo = screen.getByTestId('audiophile-logo');
     expect(audiophileLogo).toBeInTheDocument();
+  });
+  //TODO figure out if data-testid is needed
+  it('should load navbar with desktop nav items', () => {
+    render(<Navbar hideMobileProductsMenu={hideMobileProductsMenuMock} hideMobileProducts isDesktop />)
+    const homeDesktopLink = screen.getByText('Home');
+    expect(homeDesktopLink).toBeInTheDocument();
+  });
+
+  it('should load navbar without desktop nav items', () => {
+    rerender(<Navbar hideMobileProductsMenu={hideMobileProductsMenuMock} hideMobileProducts isDesktop={false} />)
+    const audiophileLogo = screen.queryByText('Home');
+    expect(audiophileLogo).not.toBeInTheDocument();
   });
 })
