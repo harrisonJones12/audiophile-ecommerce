ca022e6ea68979ad41927571f8c6714e5a0a208a
Harrison <harrisonjones5678@gmail.com>
2022-10-04 15:11:57 -0400

    setup sagas

--- src/app/sagas/sagas.js
+++ src/app/sagas/sagas.js
@@ -1,3 +1,22 @@
+import { takeLatest, all } from "redux-saga/effects";
+
 export function* helloSaga() {
     yield console.log('Hello Sagas!')
+}
+
+
+//fetch productData
+export function* fetchProductData() {
+    yield console.log('fetchProductDataSaga')
+    //logic to fetch product data
+}
+
+
+function* actionWatcher() {
+    yield takeLatest("FETCH_PRODUCT_DATA", fetchProductData);
+}
+
+export default function* rootSaga() {
+    yield all([actionWatcher()]);
+    // code after all-effect
 }
