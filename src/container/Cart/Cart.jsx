9f7d61a05818604027522122ec512bdb1399b35d
Harrison <harrisonjones5678@gmail.com>
2022-10-07 12:16:25 -0400

    WIP: Create input field component

--- src/container/Cart/Cart.jsx
+++ src/container/Cart/Cart.jsx
@@ -1,8 +1,26 @@
-import * as React from "react";
+
+import InputField from "components/InputField/InputField";
+
+const billingDetailsLabels = ["Name", "Email Address", "Phone Number"];
+
+const shippingInfoLabels = ["Your Address", "ZIP Code", "City", "Country"]
+
 export default function Cart() {
     return (
-        <>
-            <h1>Cart</h1>
-        </>
+        <div className="main-cart-container">
+            <span className="go-back-text">
+                Go Back
+            </span>
+            <div className="checkout-container">
+                <h1>Checkout</h1>
+                <form>
+                    <InputField labelText={billingDetailsLabels[0]}></InputField>
+
+                    {/* <div className="summary-container">
+                    <h1>Summary</h1>
+                </div> */}
+                </form>
+            </div>
+        </div>
     )
 }
