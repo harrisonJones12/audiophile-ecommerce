07bdc5f9d8bb16b8a34f3a7fd6a75d14cdc2c455
Harrison Jones <harrisonjones5678@gmail.com>
2024-07-11 23:31:52 -0400

    WIP

--- src/container/Home/Home.tsx
+++ src/container/Home/Home.tsx
@@ -6,9 +6,17 @@ import Productslist from "components/ProductsList/ProductsList";
 
 import FeaturedProducts from "container/FeaturedProducts/FeaturedProducts";
 
+import Footer from "container/Footer/Footer";
+
 import { fetchProducts } from "app/actions/actions";
 
 import { useAppDispatch } from "app/hooks";
+
+import mobileAboutSectionImage from "./assets/mobile/image-best-gear.jpg";
+
+import desktopAboutSectionImage from "./assets/desktop/image-best-gear.jpg";
+
+import tabletAboutSectionImage from "./assets/tablet/image-best-gear.jpg";
 
 type Props = {
   hideMobileProducts: boolean;
@@ -28,19 +36,45 @@ function Home({ hideMobileProducts, isDesktop }: Props) {
     dispatch(fetchProducts(getProductsUrl));
   }); // I added these two here to silence the eslint errors
 
+  const aboutSection = (
+    <div className="about-section-container">
+      <div className="about-image-wrapper">
+        <picture>
+          <source media="(min-width: 768px)" srcset={tabletAboutSectionImage} />
+          <source
+            media="(min-width: 1440px)"
+            srcset={desktopAboutSectionImage}
+          />
+          <img
+            src={mobileAboutSectionImage}
+            alt="man-wearing-headphones"
+            className="about-section-image"
+          />
+        </picture>
+      </div>
+      <h3 className="about-heading-text">
+        Bringing you the <span className="best-text">best</span> audio gear
+      </h3>
+      <p className="about-text">
+        Located at the heart of New York City, Audiophile is the premier store
+        for high end headphones, earphones, speakers, and audio accessories. We
+        have a large showroom and luxury demonstration rooms available for you
+        to browse and experience a wide range of our products. Stop by our store
+        to meet some of the fantastic people who make Audiophile the best place
+        to buy your portable audio equipment.
+      </p>
+    </div>
+  );
+
   return (
     <div className="Home-container" data-testid="home-container">
       {hideMobileProducts || isDesktop ? null : <div className="overlay"></div>}
-
       {/* Add home content here */}
-
       <CallToAction />
       <Productslist />
       <FeaturedProducts />
-
-      {/* shop buttons */}
-
-      {/* main content */}
+      {aboutSection}
+      <Footer />
     </div>
   );
 }
